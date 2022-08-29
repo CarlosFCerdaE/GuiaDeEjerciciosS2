b4dc50d42cf867160703577ba93a1a0324609de3
Carlos Cerda <cfcerda@uamv.edu.ni>
2022-08-28 20:23:52 -0600

    Ejercicio 2

---
+++ Ejercicio 2/Ejercicio2/src/ejercicio2/Libro.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio2;
+
+/**
+ *
+ * @author cfco5
+ */
+public class Libro {
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public Libro() {
+    }
+
+    public Libro(int id, String nombre, String descripcion, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return " | " + id + " | " + nombre + " | " + descripcion + " | " + precio + " | \n";
+    }
+    
+    
+}
